e75fe7ca6c0db7bfb07243001abde065ad78c542
GideokKim <ikid4127@naver.com>
2024-07-02 23:19:33 +0900

    feat: add files for a simple Bazel build
    - Added BUILD file to define build targets.
    - Included initial source files and Bazel build configuration.

---
+++ srcs/my_library.cc
@@ -0,0 +1,3 @@
+#include "my_library.h"
+
+std::string Simple::HelloWorld() const { return (std::string("Hello World")); }
---
+++ srcs/my_library.h
@@ -0,0 +1,14 @@
+// Greeting.hpp
+#ifndef Simple_H_
+#define Simple_H_
+
+#include <iostream>
+
+class Simple {
+ public:
+  Simple() = default;
+
+  std::string HelloWorld(void) const;
+};
+
+#endif  // Simple_H_
